d4fd2f8340b5791eb1ceb78b9712a27a1af25360
masondprice <mason.price@students.fhu.edu>
2024-02-10 20:14:22 -0600

    test

--- main.py
+++ main.py
@@ -17,3 +17,5 @@ if __name__ == '__main__':
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
     print(check_character('happy birthday!', 14))
+
+#hey
